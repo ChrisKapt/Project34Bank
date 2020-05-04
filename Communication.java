9bdc6492faca18a5dca20d05c87c8f5a068a380f
RockFreakRickerd <57324086+RockFreakRickerd@users.noreply.github.com>
2020-05-04 15:49:22 +0200

    Add files via upload
    Dit is de file die ervoor zorgt dat arduino met comport 5 met java kan communiceren

---
+++ Communication.java
@@ -0,0 +1,27 @@
+import com.fazecast.jSerialComm.SerialPort;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Communication {
+    public static void main(String[] args) {
+        SerialPort port = SerialPort.getCommPort("COM5");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        
+        if(port.openPort()){
+            System.out.println("Succesfully opened");
+        }else{
+            System.out.println("Failed to open");
+            return;
+        }
+
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0,0);
+
+        Scanner p = new Scanner(port.getInputStream());
+        while(p.hasNextLine()){
+            LocalDateTime ldt = LocalDateTime.now();
+            System.out.println(dtf.format(ldt));
+            System.out.println(p.nextLine());
+        }
+    }
+}

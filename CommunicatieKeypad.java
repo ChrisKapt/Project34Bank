42f8f9cac9b36e987f26c4abb8d08bc23587b2cc
RockFreakRickerd <57324086+RockFreakRickerd@users.noreply.github.com>
2020-06-03 15:15:37 +0200

    Add files via upload
    De besturing is er nu 100% nu alleen inloggen nog zien te doen.

--- CommunicatieKeypad.java
+++ CommunicatieKeypad.java
@@ -1,6 +1,8 @@
 package GUI;
 
 import java.io.IOException;
+
+import Database.SelectApp;
 import com.fazecast.jSerialComm.*;
 import javax.swing.*;
 import java.io.InputStream;
@@ -12,8 +14,11 @@ import java.time.format.DateTimeFormatter;
 public class CommunicatieKeypad extends JPanel{
     SerialPort sp;
     String input = "";
+    String invoer = "";
+
 
     public CommunicatieKeypad(String com ){
+        clearInput();
         sp = SerialPort.getCommPort(com);
         sp.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
 //        sp.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0); // block until bytes can be written
@@ -62,6 +67,7 @@ public class CommunicatieKeypad extends JPanel{
             System.out.println("Sent String: " + s);
             Thread.sleep(1000);
         } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
@@ -69,7 +75,7 @@ public class CommunicatieKeypad extends JPanel{
 
     public String getInput() throws InterruptedException {
 //        is niet nodig maar weerhoud wel van krijgen van 0
-        while(input == ""){
+        while(input.isEmpty()){
             Thread.sleep(1000);
 //            System.out.println("Empty");
 //            if(haveInput())
@@ -111,8 +117,9 @@ public class CommunicatieKeypad extends JPanel{
     //}
 
     public void hotkeyKeypad(JFrame frame) throws InterruptedException {
-        String frameIncome = frame.getTitle();;
+        String frameIncome = frame.getTitle();
         Balance balance;
+        WelcomeScreen welcome;
         Withdrawal_Options options;
 //      FastWithdraw fast;
         CashWithdrawl cashWithdrawl;
@@ -121,6 +128,8 @@ public class CommunicatieKeypad extends JPanel{
         System.out.println(frameIncome);
 
         System.out.println(key);
+
+        //cashWithdrawal
         if (frameIncome.equalsIgnoreCase("cashWithdrawal")) {
             //        if(key.equalsIgnoreCase("A")){
 //            fast = new Fastwithdraw();
@@ -129,38 +138,197 @@ public class CommunicatieKeypad extends JPanel{
 //        }
 
             if (key.equalsIgnoreCase("B")) {
+                closePort();
                 balance = new Balance();
-                balance.setVisible(true);
+                 balance.setVisible(true);
+                frame.setVisible(false);
 //                frame.setVisible(false);
             }
 
             if (key.equalsIgnoreCase("C")) {
+                closePort();
                 options = new Withdrawal_Options();
                 options.setVisible(true);
+                frame.setVisible(false);
 //            frame.setVisible(false);
             }
-        }
-        if (frameIncome.equalsIgnoreCase("homescreen")) {
-            if (key.equalsIgnoreCase("C")) {
-                cashWithdrawl = new CashWithdrawl();
-                cashWithdrawl.setVisible(true);
-            }
-            if(key.equalsIgnoreCase("D")){
-                balance = new Balance();
-                balance.setVisible(true);
-            }
-        }
-            if (key.equalsIgnoreCase("#")) {
+            if(key.equalsIgnoreCase("*")){
+                closePort();
                 home = new HomeScreen();
                 home.setVisible(true);
-//                frame.setVisible(false);
-//            System.out.println("homeScreen");
+                frame.setVisible(false);
             }
-            frame.setVisible(false);
-//
+            if(key.equalsIgnoreCase("#")){
+                closePort();
+                home = new HomeScreen();
+                home.setVisible(true);
+                frame.setVisible(false);
+            }
+        }
+
+        //Homescreen
+        if (frameIncome.equalsIgnoreCase("homescreen")) {
+            if (key.equalsIgnoreCase("A")) {
+                closePort();
+                cashWithdrawl = new CashWithdrawl();
+                cashWithdrawl.setVisible(true);
+                frame.setVisible(false);
+            }
+            if(key.equalsIgnoreCase("B")){
+                closePort();
+                balance = new Balance();
+                balance.setVisible(true);
+                frame.setVisible(false);
+            }
+            if(key.equalsIgnoreCase("C")){
+//            fast = new Fastwithdraw();
+//            fast.setVisible(true);
+//            frame.setVisible(false);
+            }
+            if(key.equalsIgnoreCase("#")){
+                closePort();
+                home = new HomeScreen();
+                home.setVisible(true);
+                frame.setVisible(false);
+            }
+        }
+
+        //Balance
+        if(frameIncome.equalsIgnoreCase("balance")){
+            if(key.equalsIgnoreCase("*")){
+                closePort();
+                cashWithdrawl = new CashWithdrawl();
+                cashWithdrawl.setVisible(true);
+                frame.setVisible(false);
+            }
+            if(key.equalsIgnoreCase("#")){
+                closePort();
+                home = new HomeScreen();
+                home.setVisible(true);
+                frame.setVisible(false);
+            }
+        }
+
+        if(frameIncome.equalsIgnoreCase("options")){
+            if(key.equalsIgnoreCase("*")){
+                closePort();
+                cashWithdrawl = new CashWithdrawl();
+                cashWithdrawl.setVisible(true);
+                frame.setVisible(false);
+            }
+            if(key.equalsIgnoreCase("#")){
+                closePort();
+                home = new HomeScreen();
+                home.setVisible(true);
+                frame.setVisible(false);
+            }
+        }
+
+        if(frameIncome.equalsIgnoreCase("introscreen")) {
+            if(key.equalsIgnoreCase("D")){
+                closePort();
+                 welcome = new WelcomeScreen();
+                welcome.setVisible(true);
+                frame.setVisible(false);
+            }
+//            if (input.length() == 16) {
+                //check met database
+                //if(true){
+                //welcome = new WelcomeScreen();
+                //welcome.setVisible(true);
+                //frame.setVisible(false);
+                //}
+                //else{
+                //error = new ErrorScreen();
+                //error.setVisible(true);
+                //frame.setVisible(false);
+//                }
+//            }
+            }
+
+//        if(frameIncome.equalsIgnoreCase("welcomescreen")){
+//            if(key.equalsIgnoreCase("D")){
+//                closePort();
+//                home = new HomeScreen();
+//                home.setVisible(true);
+//                frame.setVisible(false);
+//            }
+
+            // is het een key checken (asl rfid nogmaals door gestuurd wordt doet hij hier niks mee.
+            // check of het mogelijk (cijfers en geen letters)
+            // buffer += input
+            // laat zien dat er een nieuw cijfer bijgekomen is.
+            // als B ingedrukt wordt dan ga je checken met database
+            // komt pincode overeen {
+            // home = new Homescreen();
+            // closePort();
+            // home.setVisible(true);
+            // frame.setVisible(false);
+            //}else{
+            // Laten weten dat deze niet correct is
+            // if(kansen >= 3){
+            // pasBlokkeren.
+            //}
+//            if(!key.equalsIgnoreCase("A")||!key.equalsIgnoreCase("B")||!key.equalsIgnoreCase("C")||!key.equalsIgnoreCase("D")){
+//                invoer += key;
+//                //Laat zien op gui dat er een cijfer bijkomt.
+//            }
+//            if(key.equalsIgnoreCase("B")){
+//                //Check bij database
+//                if(invoer == pincode){
+//                    home = new HomeScreen();
+//                    home.setVisible(true);
+//                    frame.setVisible(false);
+//                    JPasswordField
+//                }
+//            }
+//        }
 
         clearInput();
     }
+
+//    public void inlog(JFrame frame, String user) throws InterruptedException {
+//       String currentUser = user;
+//       String password = invoer;
+//        SelectApp pwSelect = new SelectApp();
+////        String correctPass = pwSelect.selectPin(user);
+//        String correctPass = "0001";
+//            System.out.println(invoer);
+//            if (password.equals(correctPass)) {
+//                closePort();
+//                HomeScreen home = new HomeScreen();
+//                home.setVisible(true);
+//                frame.setVisible(false);
+//            } else {
+//                System.err.println("False");
+//                invoer = "";
+//            invoer += input;
+//            clearInput();
+//            System.out.println(invoer);
+//        }
+
+        //        HomeScreen home;
+//        String key = getInput();
+//        if(!key.equalsIgnoreCase("A")||!key.equalsIgnoreCase("B")||!key.equalsIgnoreCase("C")||!key.equalsIgnoreCase("D")){
+//            invoer = invoer+ key;
+//            //Laat zien op gui dat er een cijfer bijkomt.
+//        }
+//        if(key.equalsIgnoreCase("B")){
+//            //Check bij database
+//            if(invoer == pincode){
+//                closePort();
+//                home = new HomeScreen();
+//                home.setVisible(true);
+//                frame.setVisible(false);
+//                invoer = 0;
+//            }else{
+//                System.err.println("ERROR");
+//                invoer = 0;
+//            }
+//        }
+//        clearInput();
+//        System.out.println(invoer);
+//    }
 
     public boolean haveInput(){
         if (input == "") {
